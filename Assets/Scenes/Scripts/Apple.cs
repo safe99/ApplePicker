0ccd737dd6ddc59e784e031d3ae0daecaa686df4
safe99 <55143184+safe99@users.noreply.github.com>
2020-02-06 20:58:12 -0500

    Started Destroying Apple

--- Assets/Scenes/Scripts/Apple.cs
+++ Assets/Scenes/Scripts/Apple.cs
@@ -17,6 +17,9 @@ public class Apple : MonoBehaviour
         if(transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
+
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
         }
     }
 }
--- Assets/Scenes/Scripts/ApplePicker.cs
+++ Assets/Scenes/Scripts/ApplePicker.cs
@@ -31,4 +31,9 @@ public class ApplePicker : MonoBehaviour
     {
         
     }
+
+    public void AppleDestroyed()
+    {
+
+    }
 }
