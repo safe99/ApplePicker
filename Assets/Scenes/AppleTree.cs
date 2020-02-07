8868dfd4a8dbf21bf3306f92b9b99b9efe3bb23a
safe99 <55143184+safe99@users.noreply.github.com>
2020-02-06 19:31:13 -0500

    Update AppleTree.cs

--- Assets/Scenes/AppleTree.cs
+++ Assets/Scenes/AppleTree.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceToChangeDirections = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
